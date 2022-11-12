352c599585c26bffab45441fb1432e928a495f5b
Thalia Olvera <thalia_olvera@hotmail.com>
2022-11-11 22:02:55 -0500

    Resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/Cv_Thalia Olv_Dev.pdf",
+      href: "/Resume.pdf",
       style: "rounded-br-md",
       download: true,
     },
